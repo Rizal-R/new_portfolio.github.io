8ed89b09b55c947bb9f12e6efdf78d19323edef0
Rizal Rudiantoro <rizal.rudiantoro@dwp.co.id>
2022-10-10 20:54:03 +0700

    update cache

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.less';
 import { Layout, Anchor } from 'antd';
 import About from 'pages/About'
@@ -8,26 +8,33 @@ import Contact from 'pages/Contact'
 const { Header, Footer } = Layout;
 const { Link } = Anchor;
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='container'>
-        <Layout className="layout">
-          <Header>
-            <div className="logo" />
-            <Anchor targetOffset="50" >
-              <Link href="#Contact" title="Contact" />
-              <Link href="#About" title="About" />
-              {/* <Link href="#Portofolio" title="Portofolio" /> */}
-            </Anchor>
-          </Header>
-          <Contact />
-          <About />
-          {/* <Portofolio /> */}
-          <Footer style={{ textAlign: 'center' }}>Rizal Rudiantoro Portofolio ©2021</Footer>
-        </Layout>
-      </div>
-    )
-  }
+function App() {
+  useEffect(() => {
+    if ("caches" in window) {
+      caches.keys().then((names) => {
+        names.forEach((name) => {
+          caches.delete(name);
+        });
+      });
+    }
+  }, []);
+  return (
+    <div className='container'>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Anchor targetOffset="50" >
+            <Link href="#Contact" title="Contact" />
+            <Link href="#About" title="About" />
+            {/* <Link href="#Portofolio" title="Portofolio" /> */}
+          </Anchor>
+        </Header>
+        <Contact />
+        <About />
+        {/* <Portofolio /> */}
+        <Footer style={{ textAlign: 'center' }}>Rizal Rudiantoro Portofolio ©2021</Footer>
+      </Layout>
+    </div>
+  )
 }
 export default App;
